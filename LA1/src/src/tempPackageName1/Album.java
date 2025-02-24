27a52a56d2bf59927c530b6554a94d31695c4aef
2benornot2ben <bk1@arizona.edu>
2025-02-25 07:16:34 +0800

    After virtual meeting

---
+++ LA1/src/src/tempPackageName1/Album.java
@@ -0,0 +1,46 @@
+package tempPackageName1;
+
+import java.util.ArrayList;
+
+public class Album {
+	private String albumName;
+	private String artist;
+	private String genre;
+	private String year;
+	private ArrayList<Song> songs;
+	
+	public Album(String albumName, String artist, String genre, String year) {
+		this.albumName = albumName;
+		this.artist = artist;
+		this.genre = genre;
+		this.year = year;
+		this.songs = new ArrayList<Song>();
+	}
+	
+	public void addSong(Song songInst) {
+		songs.add(songInst);
+	}
+	
+	public String getAlbumName() {
+		return albumName;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	// May need to be converted to int.
+	public String getYear() {
+		return year;
+	}
+	
+	// This method might not be used.
+	// This now uses classes, so this really might not be used.
+	public ArrayList<Song> getSongList() {
+		return new ArrayList<Song>(songs);
+	}
+}
---
+++ LA1/src/src/tempPackageName1/PlayList.java
@@ -0,0 +1,39 @@
+package tempPackageName1;
+
+import java.util.ArrayList;
+
+public class PlayList {
+	private String playListName;
+	private ArrayList<Song> songs;
+	
+	public PlayList(String playListName) {
+		this.playListName = playListName;
+		this.songs = new ArrayList<Song>();
+	}
+	
+	public void addSong(Song songInst) {
+		songs.add(songInst);
+	}
+
+    // added remove song 
+	public void removeSong(Song songInst){
+        for(int i = 0; i < songs.size(); i++){
+            if(songs.get(i).getSongName().equals(songInst.getSongName())){
+                songs.remove(i);
+                break;
+            }
+        }
+    }
+
+	public String getPlayListName() {
+		return this.playListName;
+	}
+	
+	// This method might not be used.
+	// This now uses classes, so this really might not be used.
+	public ArrayList<Song> getSongList() {
+		return new ArrayList<Song>(songs);
+	}
+	
+	// We may need this to handle getting a specific song.
+}
