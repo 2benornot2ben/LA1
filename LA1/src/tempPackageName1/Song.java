7377e7b207944885a07dcbe9621b95a3c64d0496
Davranbek <kadirimbetov@arizona.edu>
2025-02-25 21:55:02 -0700

    Update Song.java

--- LA1/src/tempPackageName1/Song.java
+++ LA1/src/tempPackageName1/Song.java
@@ -1,90 +1,95 @@
+/*
+ * Added 2 getters as we need them. Fixed some bugs.
+ */
 package tempPackageName1;
 
-public class Song {
-	private String songName;
-	private String artist;
-	private String albumName;
-	private String genre;
-    //added year just in case we may need it, if not we can delete.
-    private String year;
-	private int rating;
-	private boolean favorited;
-	
-	public Song(String songName, String artist, String albumName, String genre, String year) {
-		this.songName = songName;
-		this.albumName = albumName;
-		this.artist = artist;
-		this.genre = genre;
-        this.year = year;
-        this.favorited = false;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MusicStore {
+	//instance variables
+	private ArrayList<String> fileNames;
+
+	// we may write a code for storing them in a separate file
+	private ArrayList<Album> albums;
+	private ArrayList<Song> songs;
+
+	//constructor
+	public MusicStore() throws FileNotFoundException{
+		this.fileNames = new ArrayList<String>();
+	    this.albums = new ArrayList<Album>();
+	    this.songs = new ArrayList<Song>();
+		creatingFileNames();
+		readingAlbums();
 	}
 	
-	public Song(Song incomingSong) {
-		this.songName = incomingSong.getSongName();
-		this.albumName = incomingSong.getAlbumName();
-		this.artist = incomingSong.getArtist();
-		this.genre = incomingSong.getGenre();
-        this.year = incomingSong.getYear();
-        this.favorited = incomingSong.getFavorited();
-        this.rating = incomingSong.getRating();
+	//method for creating filenames from the content of the albums.txt
+	public void creatingFileNames(){
+		// we can delete try catch and write throws FileNotFoundException to the method
+		try (Scanner scanLine = new Scanner(new File("albums.txt"))){
+			while(scanLine.hasNextLine()){
+				String line = scanLine.nextLine();
+				String[] data = line.split(",");
+				String albumFile = data[0] + "_" + data[1] + ".txt";
+				fileNames.add(albumFile);
+			}
+			scanLine.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void readingAlbums() throws FileNotFoundException{
+		for(int i = 0; i < this.fileNames.size(); i++){
+			File myFile = new File(fileNames.get(i));
+			Scanner myReader = new Scanner(myFile);
+
+			//reads only first line which is in the following format
+			//Album Title,Artist,Genre,Year
+			String header = myReader.nextLine();
+			String[] headerInfo = header.split(",");
+
+			//retrieves the data
+			String albumTitle = headerInfo[0];
+			String artist = headerInfo[1];
+			String genre = headerInfo[2];
+			String year = headerInfo[3];
+			
+			//creates and adds new album to an arrayList
+			albums.add(new Album(albumTitle, artist, genre, year));
+
+			//reading songs
+			while(myReader.hasNextLine()){
+				String songName = myReader.nextLine();
+				Song storeSong = new Song(songName, artist, albumTitle, genre, year);
+				songs.add(storeSong);
+				albums.get(i).addSong(storeSong);
+			}
+			myReader.close();
+		}
 	}
 	
-	public String getSongName() {
-		return songName;
+	public ArrayList<Song> getSongs(){
+		ArrayList<Song> copy = new ArrayList<Song>();
+	    for (Song song : songs) {
+	        copy.add(new Song(song));
+	    }
+	    return copy;
 	}
 	
-	public String getAlbumName() {
-		return albumName;
+	public ArrayList<Album> getAlbums(){
+		ArrayList<Album> copy = new ArrayList<>();
+	    for (Album album: albums) {
+	        copy.add(new Album(album));
+	    }
+	    return copy;
 	}
 	
-	public String getArtist() {
-		return artist;
-	}
-	
-	public String getGenre() {
-		return genre;
-	}
-	
-	public String getYear() {
-		return year;
-	}
-	
-	public int getRating() {
-		return rating;
-	}
-	
-	public boolean getFavorited() {
-		return favorited;
-	}
-	
-	protected void setRating(int n) {
-		if (n < 1) n = 1;
-		if (n > 5) n = 5;
-		if (n == 5) favorited = true;
-		this.rating = n;
-	}
-	
-	protected void favorite() {
-		favorited = true;
-	}
-	
-	// Feels a bit weird splitting it up like this,
-	// but it's probably more useful like this.
-	protected void unfavorite() {
-		favorited = false;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-	    if (this == obj) return true;
-	    if (obj == null || getClass() != obj.getClass()) return false;
-	    Song song = (Song) obj;
-	    return songName.equals(song.songName) &&
-	           artist.equals(song.artist) &&
-	           albumName.equals(song.albumName) &&
-	           genre.equals(song.genre) &&
-	           year.equals(song.year);
-	}
-	
-	
+	// Rating Songs goes here (Because people can rate stuff out of their library)
+	// Ability to give lists of songs/albums based on specificaitons (User can search for these via the MusicStore)
+	// Can also give non-copies of their albums/songs to Library (obviously)
 }
